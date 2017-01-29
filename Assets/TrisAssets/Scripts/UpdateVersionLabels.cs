1511fbd417dab5dc24c4657e2da70739c3e97802
avioane <muntgeor@my.dom.edu>
2017-01-29 17:48:53 -0600

    1.0.0

---
+++ Assets/TrisAssets/Scripts/UpdateVersionLabels.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateVersionLabels : MonoBehaviour {
+        
+    private string vers ="991.0.222";
+        
+    void Start () {
+        PlayerPrefs.SetString("version", vers);
+
+        GameObject.Find("LoginVersionLbl").GetComponent<Text>().text = vers;       
+
+        //GameObject.Find("LoginVersionLbl").GetComponent<Text>().text = PlayerPrefs.GetString("version");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//Canvas.
+	}
+}
